1da4b297563389a07c52c15c54d900488504f352
Nir Adler <nir.adler@komodor.io>
2025-08-06 20:43:05 +0300

    create brew tap

--- Formula/komocli.rb
+++ Formula/komocli.rb
@@ -4,10 +4,10 @@ class Komocli < Formula
   version "0.0.12"
 
   if Hardware::CPU.intel?
-    url "https://github.com/komodorio/komocli/releases/download/v0.0.12/komocli_0.0.12_darwin_amd64"
+    url "https://github.com/komodorio/komocli/releases/download/0.0.12/komocli_0.0.12_darwin_amd64"
     sha256 "8eb83bd2a7971eafdb2f783e5ee9b0ea97d4bacd04a674b907d9286b57afc510"
   elsif Hardware::CPU.arm?
-    url "https://github.com/komodorio/komocli/releases/download/v0.0.12/komocli_0.0.12_darwin_arm64"
+    url "https://github.com/komodorio/komocli/releases/download/0.0.12/komocli_0.0.12_darwin_arm64"
     sha256 "db55f1571ce0f2b39e63dcdfcf6bda619c2bc8b53809e5849b6489c6e07fb4f2"
   end
 
